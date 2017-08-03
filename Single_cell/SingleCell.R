f0bd2fff00613b8eab73d10fa67f14ad2fbab54b
DmitrySerg <sergeyev.d.a@yandex.ru>
2017-08-03 17:50:58 +0300

    paper code

---
+++ Single_cell/SingleCell.R
@@ -0,0 +1,69 @@
+library(devtools)
+install_github("satijalab/seurat")
+
+library(Seurat)
+library(dplyr)
+library(Matrix)
+
+# Load the PBMC dataset
+pbmc.data <- Read10X(data.dir = "/Users/dmitrys/Desktop/DataProjects/Bioinformatics/Single_cell/filtered_gene_bc_matrices/hg19/")
+
+
+
+# Initialize the Seurat object with the raw (non-normalized data).  Keep all
+# genes expressed in >= 3 cells (~0.1% of the data). Keep all cells with at
+# least 200 detected genes
+pbmc <- CreateSeuratObject(raw.data = pbmc.data, min.cells = 3, min.genes = 200, 
+                           project = "10X_PBMC")
+
+# The number of genes and UMIs (nGene and nUMI) are automatically calculated
+# for every object by Seurat.  For non-UMI data, nUMI represents the sum of
+# the non-normalized values within a cell We calculate the percentage of
+# mitochondrial genes here and store it in percent.mito using AddMetaData.
+# We use object@raw.data since this represents non-transformed and
+# non-log-normalized counts The % of UMI mapping to MT-genes is a common
+# scRNA-seq QC metric.  NOTE: You must have the Matrix package loaded to
+# calculate the percent.mito values.
+mito.genes <- grep(pattern = "^MT-", x = rownames(x = pbmc@data), value = TRUE)
+percent.mito <- colSums(pbmc@raw.data[mito.genes, ])/colSums(pbmc@raw.data)
+
+# AddMetaData adds columns to object@data.info, and is a great place to
+# stash QC stats
+pbmc <- AddMetaData(object = pbmc, metadata = percent.mito, col.name = "percent.mito")
+VlnPlot(object = pbmc, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
+
+
+# GenePlot is typically used to visualize gene-gene relationships, but can
+# be used for anything calculated by the object, i.e. columns in
+# object@data.info, PC scores etc.  Since there is a rare subset of cells
+# with an outlier level of high mitochondrial percentage and also low UMI
+# content, we filter these as well
+par(mfrow = c(1, 2))
+GenePlot(object = pbmc, gene1 = "nUMI", gene2 = "percent.mito")
+GenePlot(object = pbmc, gene1 = "nUMI", gene2 = "nGene")
+
+
+# We filter out cells that have unique gene counts over 2,500 or less than
+# 200 Note that low.thresholds and high.thresholds are used to define a
+# 'gate' -Inf and Inf should be used if you don't want a lower or upper
+# threshold.
+pbmc <- FilterCells(object = pbmc, subset.names = c("nGene", "percent.mito"), 
+                    low.thresholds = c(200, -Inf), high.thresholds = c(2500, 0.05))
+
+pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", 
+                      scale.factor = 10000)
+
+pbmc <- FindVariableGenes(object = pbmc, mean.function = ExpMean, dispersion.function = LogVMR, 
+                          x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
+
+
+pbmc <- ScaleData(object = pbmc, vars.to.regress = c("nUMI", "percent.mito"))
+
+pbmc <- RunPCA(object = pbmc, pc.genes = pbmc@var.genes, do.print = TRUE, pcs.print = 1:5, 
+               genes.print = 5)
+
+# Examine and visualize PCA results a few different ways
+PrintPCA(object = pbmc, pcs.print = 1:5, genes.print = 5, use.full = FALSE)
+
+VizPCA(object = pbmc, pcs.use = 1:2)
+PCAPlot(object = pbmc, dim.1 = 1, dim.2 = 2)
