28a26fdaa881b75c555c58cea44ccfeaceaf9623
DmitrySerg <sergeyev.d.a@yandex.ru>
2017-08-03 20:33:06 +0300

    paper code

--- Single_cell/SingleCell.R
+++ Single_cell/SingleCell.R
@@ -6,7 +6,9 @@ library(dplyr)
 library(Matrix)
 
 # Load the PBMC dataset
-pbmc.data <- Read10X(data.dir = "/Users/dmitrys/Desktop/DataProjects/Bioinformatics/Single_cell/filtered_gene_bc_matrices/hg19/")
+file_location <- "/Users/dmitrys/Desktop/DataProjects/Bioinformatics/Single_cell/"
+
+pbmc.data <- Read10X(data.dir = "/Users/dmitrys/Desktop/DataProjects/Bioinformatics/Single_cell/GRCh38/")
 
 
 
@@ -67,3 +69,85 @@ PrintPCA(object = pbmc, pcs.print = 1:5, genes.print = 5, use.full = FALSE)
 
 VizPCA(object = pbmc, pcs.use = 1:2)
 PCAPlot(object = pbmc, dim.1 = 1, dim.2 = 2)
+
+
+# ProjectPCA scores each gene in the dataset (including genes not included
+# in the PCA) based on their correlation with the calculated components.
+# Though we don't use this further here, it can be used to identify markers
+# that are strongly correlated with cellular heterogeneity, but may not have
+# passed through variable gene selection.  The results of the projected PCA
+# can be explored by setting use.full=T in the functions above
+pbmc <- ProjectPCA(object = pbmc, do.print = FALSE)
+PCHeatmap(object = pbmc, pc.use = 1, cells.use = 500, do.balanced = TRUE, label.columns = FALSE)
+
+
+PCHeatmap(object = pbmc, pc.use = 1:12, cells.use = 500, do.balanced = TRUE, 
+          label.columns = FALSE, use.full = FALSE)
+
+# NOTE: This process can take a long time for big datasets, comment out for
+# expediency.  More approximate techniques such as those implemented in
+# PCElbowPlot() can be used to reduce computation time
+pbmc <- JackStraw(object = pbmc, num.replicate = 100, do.print = FALSE)
+
+JackStrawPlot(object = pbmc, PCs = 1:12)
+PCElbowPlot(object = pbmc)
+
+
+# save.SNN = T saves the SNN so that the clustering algorithm can be rerun
+# using the same graph but with a different resolution value (see docs for
+# full details)
+pbmc <- FindClusters(object = pbmc, reduction.type = "pca", dims.use = 1:10, 
+                     resolution = 0.6, print.output = 0, save.SNN = TRUE)
+PrintFindClustersParams(object = pbmc)
+
+pbmc <- RunTSNE(object = pbmc, dims.use = 1:10, do.fast = TRUE)
+TSNEPlot(object = pbmc)
+
+save(pbmc, file = "/Users/dmitrys/Desktop/DataProjects/Bioinformatics/Single_cell/singleCell.Robj")
+
+# find all markers of cluster 1
+cluster1.markers <- FindMarkers(object = pbmc, ident.1 = 1, min.pct = 0.25)
+print(x = head(x = cluster1.markers, n = 5))
+
+
+# find all markers distinguishing cluster 5 from clusters 0 and 3
+cluster5.markers <- FindMarkers(object = pbmc, ident.1 = 5, ident.2 = c(0, 3), 
+                                min.pct = 0.25)
+print(x = head(x = cluster5.markers, n = 5))
+
+
+# find markers for every cluster compared to all remaining cells, report
+# only the positive ones
+pbmc.markers <- FindAllMarkers(object = pbmc, only.pos = TRUE, min.pct = 0.25, 
+                               thresh.use = 0.25)
+pbmc.markers %>% group_by(cluster) %>% top_n(2, avg_diff)
+
+
+cluster1.markers <- FindMarkers(object = pbmc, ident.1 = 0, thresh.use = 0.25, 
+                                test.use = "roc", only.pos = TRUE)
+
+VlnPlot(object = pbmc, features.plot = c("MS4A1", "CD79A"))
+
+# you can plot raw UMI counts as well
+VlnPlot(object = pbmc, features.plot = c("NKG7", "PF4"), use.raw = TRUE, y.log = TRUE)
+
+
+FeaturePlot(object = pbmc, features.plot = c("MS4A1", "GNLY", "CD3E", "CD14", 
+                                             "FCER1A", "FCGR3A", "LYZ", "PPBP", "CD8A"), cols.use = c("grey", "blue"), 
+            reduction.use = "tsne")
+
+
+top10 <- pbmc.markers %>% group_by(cluster) %>% top_n(10, avg_diff)
+# setting slim.col.label to TRUE will print just the cluster IDS instead of
+# every cell name
+DoHeatmap(object = pbmc, genes.use = top10$gene, order.by.ident = TRUE, slim.col.label = TRUE, 
+          remove.key = TRUE)
+
+
+current.cluster.ids <- c(0, 1, 2, 3, 4, 5, 6, 7)
+new.cluster.ids <- c("CD4 T cells", "CD14+ Monocytes", "B cells", "CD8 T cells", 
+                     "FCGR3A+ Monocytes", "NK cells", "Dendritic cells", "Megakaryocytes")
+pbmc@ident <- plyr::mapvalues(x = pbmc@ident, from = current.cluster.ids, to = new.cluster.ids)
+TSNEPlot(object = pbmc, do.label = TRUE, pt.size = 0.5)
+
+
